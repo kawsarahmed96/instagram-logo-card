47206bcc176804df9879037e29f2958c72d0feb6
kawsarahmed96 <fl.kawsarahmed@gmail.com>
2024-08-11 16:19:32 +0600

    logo card design using react

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import InstagramLogo from "./instagram/InstagramLogo";
+
+function App() {
+  return (
+    <>
+      <InstagramLogo />
+    </>
+  );
+}
+
+export default App;
---
+++ src/instagram/LogoBody.jsx
@@ -0,0 +1,22 @@
+const LogoBody = () => {
+  return (
+    <div className="border">
+      <div className="cardArea">
+        <h3>Instagram</h3>
+        <div className="">
+          <img
+            src="https://scontent.fjsr13-1.fna.fbcdn.net/v/t39.30808-1/333936078_1361956414655948_834213367116914942_n.jpg?stp=c1.16.176.175a_dst-jpg_p240x240&_nc_cat=102&ccb=1-7&_nc_sid=0ecb9b&_nc_eui2=AeHmGrivgmiC0jtCgwAZs_56kYHgC5X85UeRgeALlfzlR7qfLwpgfB0dA_MNNAlNRh5J_kgxb8kC77g6dT6B1AR_&_nc_ohc=T3Gm9uTmYPYQ7kNvgFaXRrn&_nc_ht=scontent.fjsr13-1.fna&oh=00_AYB9MfHGkdM3oqDKzUDCELSUzjIj9OYJthCEcZVn-6a_xw&oe=66BE0A17"
+            alt=""
+          />
+        </div>
+        <button className="name">Continue as_ashraful_haque</button>
+        <ul>
+          <li>not Ashraful haque?</li>
+          <li>Switch accoount</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default LogoBody;
